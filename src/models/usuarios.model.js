b2ee64e51ef065104d758d5529fc6f1ab30d0437
jDiego18 <108305478+jDiego18@users.noreply.github.com>
2025-10-21 13:56:32 -0600

    Feature/registrar usuario (#4)
    * Contraseña no obligatoria
    
     - Se elimina la validación de la contraseña al crear un usuario
    
    * Ajuste modelo y validaciones de usuario
    
    - Se corrigen el nombre de los campos, para que estén iguales a la base de datos
    - Se agregan campos adicionales, Creado_Por_Id, Fecha_Creacion, Estatus_Id

--- src/models/usuarios.model.js
+++ src/models/usuarios.model.js
@@ -23,9 +23,14 @@ const Usuario = {
             .input('Nombre', sql.NVarChar, user.Nombre)
             .input('Correo', sql.NVarChar, user.Correo)
             .input('Contraseña', sql.NVarChar, user.Contraseña)
-            .input('Rol', sql.Int, user.Rol)
-            .query(`INSERT INTO ${table} (Nombre, Correo, Contraseña, Rol) VALUES (@Nombre, @Correo, @Contraseña, @Rol);
-                    SELECT SCOPE_IDENTITY() AS Id;`);
+            .input('Rol_Id', sql.Int, user.Rol_Id)
+            .input('Creado_Por_Id', sql.Int, user.Creado_Por_Id)
+            .input('Fecha_Creacion', sql.DateTime, new Date())
+            .input('Estatus_Id', sql.Int, 1)
+            .query(`INSERT INTO ${table} 
+                (Nombre, Correo, Contraseña, Rol_Id, Creado_Por_Id, Fecha_Creacion, Estatus_Id) 
+                VALUES (@Nombre, @Correo, @Contraseña, @Rol_Id, @Creado_Por_Id, @Fecha_Creacion, @Estatus_Id);
+                SELECT SCOPE_IDENTITY() AS Id;`);
         return { id: result.recordset[0].Id, ...user };
     },
 
@@ -36,9 +41,9 @@ const Usuario = {
             .input('Nombre', sql.NVarChar, user.Nombre)
             .input('Correo', sql.NVarChar, user.Correo)
             .input('Contraseña', sql.NVarChar, user.Contraseña)
-            .input('Rol', sql.Int, user.Rol)
-            .input('Estatus', sql.Int, user.Estatus)
-            .query(`UPDATE ${table} SET Nombre = @Nombre, Correo = @Correo, Contraseña = @Contraseña, Rol = @Rol, Estatus = @Estatus WHERE Usuarios_Id = @Id`);
+            .input('Rol_Id', sql.Int, user.Rol_Id)
+            .input('Estatus_Id', sql.Int, user.Estatus_Id)
+            .query(`UPDATE ${table} SET Nombre = @Nombre, Correo = @Correo, Contraseña = @Contraseña, Rol_Id = @Rol_Id, Estatus_Id = @Estatus_Id WHERE Usuarios_Id = @Id`);
         return result.rowsAffected[0] > 0 ? { id, ...user } : null;
     },
 
--- src/validators/usuarios.validator.js
+++ src/validators/usuarios.validator.js
@@ -1,17 +1,17 @@
 import {body, param} from "express-validator";
 
 export const createUsuarioValidator = [
-    body('nombre').isString().withMessage('El nombre es obligatorio.'),
-    body('correo').isEmail().withMessage('El correo es obligatorio y debe ser un correo válido.'),
-    body('contraseña').isLength({min: 6}).withMessage('La contraseña es obligatoria y debe tener al menos 6 caracteres.'),
-    body('rol').isInt().withMessage('El rol es obligatorio.')
+    body('Nombre').isString().withMessage('El nombre es obligatorio.'),
+    body('Correo').isEmail().withMessage('El correo es obligatorio y debe ser un correo válido.'),
+    body('Rol_Id').isInt().withMessage('El rol es obligatorio.'),
+    body('Creado_Por_Id').isInt().withMessage('El campo creado por es obligatorio.')
 ];
 
 export const updateUsuarioValidator = [
     param('id').isInt().withMessage('El ID del usuario debe ser un número entero.'),
-    body('nombre').isString().withMessage('El nombre es obligatorio.'),
-    body('correo').isEmail().withMessage('El correo es obligatorio y debe ser un correo válido.'),
-    body('contraseña').isLength({min: 6}).withMessage('La contraseña es obligatoria y debe tener al menos 6 caracteres.'),
-    body('rol').isInt().withMessage('El rol es obligatorio.'),
-    body('estatus').isInt().withMessage('El estatus es obligatorio.')
+    body('Nombre').isString().withMessage('El nombre es obligatorio.'),
+    body('Correo').isEmail().withMessage('El correo es obligatorio y debe ser un correo válido.'),
+    body('Contraseña').isLength({min: 6}).withMessage('La contraseña es obligatoria y debe tener al menos 6 caracteres.'),
+    body('Rol_Id').isInt().withMessage('El rol es obligatorio.'),
+    body('Estatus_Id').isInt().withMessage('El estatus es obligatorio.')
 ];
