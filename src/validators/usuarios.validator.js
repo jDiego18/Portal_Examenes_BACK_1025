9df23c1fa7ac9aff618697d0d168596ea42ed19a
jDiego18 <juand18pumascatarina@gmail.com>
2025-10-21 13:22:18 -0600

    Contraseña no obligatoria
    Se elimina la validación de la contraseña al crear un usuario

--- src/validators/usuarios.validator.js
+++ src/validators/usuarios.validator.js
@@ -3,7 +3,6 @@ import {body, param} from "express-validator";
 export const createUsuarioValidator = [
     body('nombre').isString().withMessage('El nombre es obligatorio.'),
     body('correo').isEmail().withMessage('El correo es obligatorio y debe ser un correo válido.'),
-    body('contraseña').isLength({min: 6}).withMessage('La contraseña es obligatoria y debe tener al menos 6 caracteres.'),
     body('rol').isInt().withMessage('El rol es obligatorio.')
 ];
 
